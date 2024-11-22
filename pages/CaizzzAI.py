910dea81bffd05b1b7d5ce8c1f06e6921a8f7dd2
陈展鹏 <138754853+TITOCHAN2023@users.noreply.github.com>
2024-11-22 13:39:59 +0800

    Delete pages/CaizzzAI.py

--- pages/CaizzzAI.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="CaizzzAI", page_icon="🧊", layout="wide"
-                   , initial_sidebar_state="expanded")
-
-st.title("CaizzzAI Settings")
-
-st.text_input("API Key", value=st.session_state["OPENAI_API_KEY"], max_chars=None, key=None, type='default')
